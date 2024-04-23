b30740a2bcbbf11e926e6d2672183e1d5e405549
FelipeMiguel0 <144391022+FelipeMiguel0@users.noreply.github.com>
2024-04-23 06:13:36 -0300

    Commit inicial

---
+++ src/js/index.js
@@ -0,0 +1,23 @@
+
+const listaSelecaoPokemons = document.querySelectorAll('.pokemon')
+const pokemonsCard = document.querySelectorAll('.cartao-pokemon')
+
+listaSelecaoPokemons.forEach(pokemon =>{
+
+    pokemon.addEventListener('click', () => {
+         const cartaoPokemonAberto = document.querySelector('.aberto')
+         cartaoPokemonAberto.classList.remove('aberto')
+
+        const idPokemonSelecionado = pokemon.attributes.id.value
+
+        const idCartaoPokemonParaAbrir = 'cartao-' + idPokemonSelecionado
+        const cartaoPokemonParaAbrir = document.getElementById(idCartaoPokemonParaAbrir)
+        cartaoPokemonParaAbrir.classList.add('aberto')
+
+        const pokemonAtivoNaListagem = document.querySelector('.ativo')
+         pokemonAtivoNaListagem.classList.remove('ativo')
+
+         const pokemonSelicionadoNaListagem = document.getElementById(idPokemonSelecionado)
+         pokemonSelicionadoNaListagem.classList.add('ativo')
+    })
+})
